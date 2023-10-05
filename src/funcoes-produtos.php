c07f6e80f66a5365bab02e5b6dee002dc8c4c697
Kauê Evangelista <kauesilva38@hotmail.com>
2023-10-05 12:01:27 -0300

    comit

--- src/funcoes-produtos.php
+++ src/funcoes-produtos.php
@@ -26,65 +26,65 @@ require_once "conecta.php";
 
 
 
-function inserirProduto(
-    PDO $conexao, string $nome, float $preco, 
-    int $quantidade, int $fabricanteId, string $descricao ):void {
+// function inserirProduto(
+//     PDO $conexao, string $nome, float $preco, 
+//     int $quantidade, int $fabricanteId, string $descricao ):void {
 
-    $sql = "INSERT INTO produtos(
-        nome, preco, quantidade, descricao, fabricante_id
-    ) VALUES(
-        :nome, :preco, :quantidade, :descricao, :fabricanteId
-    )";    
+//     $sql = "INSERT INTO produtos(
+//         nome, preco, quantidade, descricao, fabricante_id
+//     ) VALUES(
+//         :nome, :preco, :quantidade, :descricao, :fabricanteId
+//     )";    
 
-    try {
-        $consulta = $conexao->prepare($sql);
-        $consulta->bindValue(":nome", $nome, PDO::PARAM_STR);
-        $consulta->bindValue(":preco", $preco, PDO::PARAM_STR);
-        $consulta->bindValue(":quantidade", $quantidade, PDO::PARAM_INT);
-        $consulta->bindValue(":descricao", $descricao, PDO::PARAM_STR);
-        $consulta->bindValue(":fabricanteId", $fabricanteId, PDO::PARAM_INT);
-        $consulta->execute();
-    } catch (Exception $erro) {
-        die("Erro ao inserir: ".$erro->getMessage());
-    }
-}
-
-
-function lerUmProduto(PDO $conexao, int $id):array {
-    $sql = "SELECT * FROM produtos WHERE id = :id";
-    try {
-        $consulta = $conexao->prepare($sql);
-        $consulta->bindValue(":id", $id, PDO::PARAM_INT);
-        $consulta->execute();
-        $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
-    } catch (Exception $erro) {
-        die("Erro ao carregar dados: ".$erro->getMessage());
-    }    
-    return $resultado;
-}
+//     try {
+//         $consulta = $conexao->prepare($sql);
+//         $consulta->bindValue(":nome", $nome, PDO::PARAM_STR);
+//         $consulta->bindValue(":preco", $preco, PDO::PARAM_STR);
+//         $consulta->bindValue(":quantidade", $quantidade, PDO::PARAM_INT);
+//         $consulta->bindValue(":descricao", $descricao, PDO::PARAM_STR);
+//         $consulta->bindValue(":fabricanteId", $fabricanteId, PDO::PARAM_INT);
+//         $consulta->execute();
+//     } catch (Exception $erro) {
+//         die("Erro ao inserir: ".$erro->getMessage());
+//     }
+// }
 
 
-function atualizarProduto(PDO $conexao, int $id, string $nome, float $preco, int $quantidade, string $descricao, int $fabricanteId):void {
+// function lerUmProduto(PDO $conexao, int $id):array {
+//     $sql = "SELECT * FROM produtos WHERE id = :id";
+//     try {
+//         $consulta = $conexao->prepare($sql);
+//         $consulta->bindValue(":id", $id, PDO::PARAM_INT);
+//         $consulta->execute();
+//         $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+//     } catch (Exception $erro) {
+//         die("Erro ao carregar dados: ".$erro->getMessage());
+//     }    
+//     return $resultado;
+// }
 
-    $sql = "UPDATE produtos SET
-        nome = :nome,
-        preco = :preco,
-        quantidade = :quantidade,
-        descricao = :descricao,
-        fabricante_id = :fabricanteId WHERE id = :id";
-    try {
-        $consulta = $conexao->prepare($sql);
-        $consulta->bindValue(":nome", $nome, PDO::PARAM_STR);
-        $consulta->bindValue(":preco", $preco, PDO::PARAM_STR);
-        $consulta->bindValue(":quantidade", $quantidade, PDO::PARAM_INT);
-        $consulta->bindValue(":descricao", $descricao, PDO::PARAM_STR);
-        $consulta->bindValue(":fabricanteId", $fabricanteId, PDO::PARAM_INT);
-        $consulta->bindValue(":id", $id, PDO::PARAM_INT);
-        $consulta->execute();
-    } catch (Exception $erro) {
-        die("Erro ao atualizar: ".$erro->getMessage());
-    }   
-}
+
+// function atualizarProduto(PDO $conexao, int $id, string $nome, float $preco, int $quantidade, string $descricao, int $fabricanteId):void {
+
+//     $sql = "UPDATE produtos SET
+//         nome = :nome,
+//         preco = :preco,
+//         quantidade = :quantidade,
+//         descricao = :descricao,
+//         fabricante_id = :fabricanteId WHERE id = :id";
+//     try {
+//         $consulta = $conexao->prepare($sql);
+//         $consulta->bindValue(":nome", $nome, PDO::PARAM_STR);
+//         $consulta->bindValue(":preco", $preco, PDO::PARAM_STR);
+//         $consulta->bindValue(":quantidade", $quantidade, PDO::PARAM_INT);
+//         $consulta->bindValue(":descricao", $descricao, PDO::PARAM_STR);
+//         $consulta->bindValue(":fabricanteId", $fabricanteId, PDO::PARAM_INT);
+//         $consulta->bindValue(":id", $id, PDO::PARAM_INT);
+//         $consulta->execute();
+//     } catch (Exception $erro) {
+//         die("Erro ao atualizar: ".$erro->getMessage());
+//     }   
+// }
 
 
 function excluirProduto(PDO $conexao, int $id):void {
